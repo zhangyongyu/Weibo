e4da367408ec4fa2011c3c19aaedf39038f8328d
zhangyongyu <zhangyongyu1024@gmail.com>
2017-10-30 18:28:07 +0000

    test

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('/signup', 'UsersController@create')->name('signup');
-
+Route::get('test', function() {
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv('DATABASE_URL'));
+        var_dump($url);
+});
 
 Route::resource('users', 'UsersController');
