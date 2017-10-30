fc0e4086c7d76893fe791138bc8218b6c00394e7
zhangyongyu <zhangyongyu1024@gmail.com>
2017-10-30 10:06:07 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>@yield('title', 'Sample') -- Nicole</title>
-    </head>
-    <body>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
         @yield('content')
-    </body>
+        @include('layouts._footer')
+      </div>
+    </div>
+  </body>
 </html>
